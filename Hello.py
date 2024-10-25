c12795e27c9290e4ac06fda2d997dd38f93e1133
Monish Nule <imonishnule8@gmail.com>
2024-10-25 14:11:24 +0530

    Testing Pycharm

---
+++ Hello.py
@@ -0,0 +1,11 @@
+print("Hello World")
+import matplotlib.pyplot as plt
+
+x = [1,2,3,4,5]
+y = [4,5,6,7,8]
+
+plt.plot(x,y)
+plt.title("Sample Plot")
+plt.xlabel("X-Axis")
+plt.ylabel("Y-Axis")
+plt.show()
